e6da66004ea11e3a4da57337b35c578f4b23f323
Fabian Kübler <fabian.kuebler@zweitag.de>
2018-02-17 00:48:27 +0100

    fix runfile

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ mount -t devtmpfs none /dev
 
 echo "Starting subprocesses..."
 dumb-init \
-  redis-server && \
-  sidekiq -c 10 -r /usr/src/app/boot.rb && \
-  sidekiq -c 4 -q ocr -r /usr/src/app/boot.rb && \
+  redis-server & \
+  sidekiq -c 10 -r /usr/src/app/boot.rb & \
+  sidekiq -c 4 -q ocr -r /usr/src/app/boot.rb & \
   SANE_CONFIG_DIR=/usr/local/etc/scanbd /usr/local/sbin/scanbd -f -c /usr/local/etc/scanbd/scanbd.conf
