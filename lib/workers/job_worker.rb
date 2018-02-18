a892465327317754005261d951b50955c72ef8de
Fabian Kübler <fabian.kuebler@zweitag.de>
2018-02-18 14:03:58 +0100

    allow one concurrent job simultaneously

--- lib/workers/job_worker.rb
+++ lib/workers/job_worker.rb
@@ -2,6 +2,8 @@ module Workers
   class JobWorker
     include Sidekiq::Worker
 
+    sidekiq_options queue: :job
+
     def perform(options)
       directory = Directory.from_h(options.fetch('directory'))
       Workflows::Job.new(directory).perform
--- lib/workers/page_worker.rb
+++ lib/workers/page_worker.rb
@@ -2,7 +2,7 @@ module Workers
   class PageWorker
     include Sidekiq::Worker
 
-    sidekiq_options queue: :ocr
+    sidekiq_options queue: :page
 
     def perform(path)
       Workflows::Page.new(path: Pathname.new(path)).process
